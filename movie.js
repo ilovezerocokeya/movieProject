f4a23305caf96510c22bd9f8e0f9db846fc47d75
ilovezerocokeya <147008077a@gmail.com>
2024-04-29 14:56:57 +0900

    일단 제출

--- movie.js
+++ movie.js
@@ -23,8 +23,31 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzVkYjYzMTZhNDMzYTU3ZjY5ODBmN2FkYTcxYTczMiIsInN1YiI6IjY2MmQwOGNlYTgwNjczMDEyMmU4MzQ2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XG5lDAMMqpMSXOqvhyHp3_BEUAHKeRxTllhvKAJ-hF4'
     }
   };
-  
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+
+  const movieCards = document.querySelector('.movieCards')
+
+  movieCards.innerHTML = '';
+
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .then(data => {
+        const content = document.getElementById('content');
+        console.log(data);
+
+            const cardArray = data.results
+            cardArray.forEach(card => {
+              movieCards.innerHTML += `
+              <div id="content" class="container">                  
+              <div class="card">
+                    <img src="https://image.tmdb.org/t/p/w300/${card.poster_path}"  alt="영화이미지" />
+                    <h3 class="title">${card.title}</h3>
+                    <p class="overview">${card.overview}</p>
+                    <p class="rating">${card.vote_average}</p>
+                  </div>
+                </div>`
+             });
+            
+    });
+    
+
+
